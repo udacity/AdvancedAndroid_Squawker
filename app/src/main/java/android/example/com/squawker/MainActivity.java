548043b97993298bdd0fb1cb35a54f8417b92844
Lyla <lyla@udacity.com>
2017-02-27 17:58:47 -0800

    TFCM.04-Solution-AddFirebaseMessagingService

--- app/src/main/java/android/example/com/squawker/MainActivity.java
+++ app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -88,39 +88,6 @@ public class MainActivity extends AppCompatActivity implements
         // Start the loader
         getSupportLoaderManager().initLoader(LOADER_ID_MESSAGES, null, this);
 
-
-        // TODO (1) Make a new Service in the fcm package that extends from FirebaseMessagingService.
-            // TODO (2) As part of the new Service - Override onMessageReceived. This method will
-            // be triggered whenever a squawk is received. You can get the data from the squawk
-            // message using getData(). When you send a test message, this data will include the
-            // following key/value pairs:
-                // test: true
-                // author: Ex. "TestAccount"
-                // authorKey: Ex. "key_test"
-                // message: Ex. "Hello world"
-                // date: Ex. 1484358455343
-            // TODO (3) As part of the new Service - If there is message data, get the data using
-            // the keys and do two things with it :
-                // 1. Display a notification with the first 30 character of the message
-                // 2. Use the content provider to insert a new message into the local database
-                // Hint: You shouldn't be doing content provider operations on the main thread.
-                // If you don't know how to make notifications or interact with a content provider
-                // look at the notes in the classroom for help.
-
-
-        // TODO (5) You can delete the code below for getting the extras from a notification message,
-        // since this was for testing purposes and not part of Squawker.
-        
-        // Gets the extra data from the intent that started the activity. For *notification*
-        // messages, this will contain key value pairs stored in the *data* section of the message.
-        Bundle extras = getIntent().getExtras();
-        // Checks if the extras exist and if the key "test" from our FCM message is in the intent
-        if (extras != null && extras.containsKey("test")) {
-            // If the key is there, print out the value of "test"
-            Log.d(LOG_TAG, "Contains: " + extras.getString("test"));
-        }
-
-
         // Get token from the ID Service you created and show it in a log
         String token = FirebaseInstanceId.getInstance().getToken();
         String msg = getString(R.string.message_token_format, token);
