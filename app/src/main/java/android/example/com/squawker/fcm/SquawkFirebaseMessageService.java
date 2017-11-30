37f09e23a1a34daaf5afa2665a9e06e2bcdc923d
Hrishikesh Kadam <hrkadam.92@gmail.com>
2017-12-01 01:23:52 +0530

    TFCM.04-Solution-AddFirebaseMessagingService

---
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseMessageService.java
@@ -0,0 +1,182 @@
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package android.example.com.squawker.fcm;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.example.com.squawker.MainActivity;
+import android.example.com.squawker.R;
+import android.example.com.squawker.provider.SquawkContract;
+import android.example.com.squawker.provider.SquawkProvider;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+/**
+ * Listens for squawk FCM messages both in the background and the foreground and responds
+ * appropriately
+ * depending on type of message
+ */
+public class SquawkFirebaseMessageService extends FirebaseMessagingService {
+
+    private static final String JSON_KEY_AUTHOR = SquawkContract.COLUMN_AUTHOR;
+    private static final String JSON_KEY_AUTHOR_KEY = SquawkContract.COLUMN_AUTHOR_KEY;
+    private static final String JSON_KEY_MESSAGE = SquawkContract.COLUMN_MESSAGE;
+    private static final String JSON_KEY_DATE = SquawkContract.COLUMN_DATE;
+
+    private static final int NOTIFICATION_MAX_CHARACTERS = 30;
+    private static String LOG_TAG = SquawkFirebaseMessageService.class.getSimpleName();
+
+    // TODO COMPLETED (1) Make a new Service in the fcm package that extends from FirebaseMessagingService.
+    // TODO COMPLETED (2) As part of the new Service - Override onMessageReceived. This method will
+    // be triggered whenever a squawk is received. You can get the data from the squawk
+    // message using getData(). When you send a test message, this data will include the
+    // following key/value pairs:
+    // test: true
+    // author: Ex. "TestAccount"
+    // authorKey: Ex. "key_test"
+    // message: Ex. "Hello world"
+    // date: Ex. 1484358455343
+    // TODO COMPLETED (3) As part of the new Service - If there is message data, get the data using
+    // the keys and do two things with it :
+    // 1. Display a notification with the first 30 character of the message
+    // 2. Use the content provider to insert a new message into the local database
+    // Hint: You shouldn't be doing content provider operations on the main thread.
+    // If you don't know how to make notifications or interact with a content provider
+    // look at the notes in the classroom for help.
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // There are two types of messages data messages and notification messages. Data messages
+        // are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data
+        // messages are the type
+        // traditionally used with FCM. Notification messages are only received here in
+        // onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated
+        // notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages
+        // containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always
+        // sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options\
+
+        // The Squawk server always sends just *data* messages, meaning that onMessageReceived when
+        // the app is both in the foreground AND the background
+
+        Log.d(LOG_TAG, "-> From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+
+        Map<String, String> data = remoteMessage.getData();
+
+        if (data.size() > 0) {
+            Log.d(LOG_TAG, "-> Message data payload: " + data);
+
+            // Send a notification that you got a new message
+            sendNotification(data);
+            insertSquawk(data);
+
+        }
+    }
+
+    /**
+     * Inserts a single squawk into the database;
+     *
+     * @param data Map which has the message data in it
+     */
+    private void insertSquawk(final Map<String, String> data) {
+
+        // Database operations should not be done on the main thread
+        AsyncTask<Void, Void, Void> insertSquawkTask = new InsertSquawkTask(this, data);
+        insertSquawkTask.execute();
+    }
+
+    private static class InsertSquawkTask extends AsyncTask<Void, Void, Void> {
+
+        private Context context;
+        private Map<String, String> data;
+
+        public InsertSquawkTask(Context context, Map<String, String> data) {
+            this.context = context;
+            this.data = data;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            ContentValues newMessage = new ContentValues();
+            newMessage.put(SquawkContract.COLUMN_AUTHOR, data.get(JSON_KEY_AUTHOR));
+            newMessage.put(SquawkContract.COLUMN_MESSAGE, data.get(JSON_KEY_MESSAGE).trim());
+            newMessage.put(SquawkContract.COLUMN_DATE, data.get(JSON_KEY_DATE));
+            newMessage.put(SquawkContract.COLUMN_AUTHOR_KEY, data.get(JSON_KEY_AUTHOR_KEY));
+            context.getContentResolver().insert(SquawkProvider.SquawkMessages.CONTENT_URI, newMessage);
+            context = null;
+            return null;
+        }
+    }
+
+
+    /**
+     * Create and show a simple notification containing the received FCM message
+     *
+     * @param data Map which has the message data in it
+     */
+    private void sendNotification(Map<String, String> data) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // Create the pending intent to launch the activity
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String author = data.get(JSON_KEY_AUTHOR);
+        String message = data.get(JSON_KEY_MESSAGE);
+
+        // If the message is longer than the max number of characters we want in our
+        // notification, truncate it and add the unicode character for ellipsis
+        if (message.length() > NOTIFICATION_MAX_CHARACTERS) {
+            message = message.substring(0, NOTIFICATION_MAX_CHARACTERS) + "\u2026";
+        }
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_duck)
+                .setContentTitle(String.format(getString(R.string.notification_message), author))
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
