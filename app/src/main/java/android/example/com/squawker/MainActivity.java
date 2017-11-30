01eee1e2a53bed2b54cd42583eae4f160485a448
Hrishikesh Kadam <hrkadam.92@gmail.com>
2017-11-30 22:08:01 +0530

    TFCM.03-Exercise-GetInstanceIdToken

--- app/src/main/java/android/example/com/squawker/MainActivity.java
+++ app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -87,7 +87,6 @@ public class MainActivity extends AppCompatActivity implements
         // Start the loader
         getSupportLoaderManager().initLoader(LOADER_ID_MESSAGES, null, this);
 
-        // TODO COMPLETED (1) Get the test data here from the extras bundle that came with this intent.
         // Gets the extra data from the intent that started the activity. For *notification*
         // messages, this will contain key value pairs stored in the *data* section of the message.
         Bundle extras = getIntent().getExtras();
@@ -96,6 +95,13 @@ public class MainActivity extends AppCompatActivity implements
             // If the key is there, print out the value of "test"
             Log.d(LOG_TAG, "-> Contains: " + extras.getString("test"));
         }
+
+        // TODO (1) Make a new package for your FCM service classes called "fcm"
+        // TODO (2) Create a new Service class that extends FirebaseInstanceIdService.
+        // You'll need to implement the onTokenRefresh method. Simply have it print out
+        // the new token.
+        // TODO (3) Here, in MainActivity, get a token using FirebaseInstanceId.getInstance().getToken()
+        // TODO (4) Get the message from that token and print it in a log statement
 
     }
 
