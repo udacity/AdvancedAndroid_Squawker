a07aace5a6de50cc5431806359ae93cd0c168414
Doilio Matsinhe <38020305+doilio@users.noreply.github.com>
2019-02-04 20:30:57 +0200

    Update MainActivity.java
    Update so that you could see the changes happening in the recycleView at Real time, without the need no exit and open the app or rotate the screen.

--- app/src/main/java/android/example/com/squawker/MainActivity.java
+++ app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -132,6 +132,7 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         mAdapter.swapCursor(data);
+        mAdapter.notifyDataSetChanged();
     }
 
     @Override
